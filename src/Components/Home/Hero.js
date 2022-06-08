6fe640369d5d4f19d091846357648c907d460f40
intisar eusha <iftikharrasha@gmail.com>
2022-06-08 11:18:11 +0600

    hero section added

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -4,6 +4,27 @@ const Hero = () => {
     return (
             <>
                 <section className="hero">
+                        <div className="wrapper hero__contents">
+                            <h1>TV en vivo, películas, series y más…</h1>
+                            <h2>Navega con la experiencia YOY sin costo</h2>
+                            <ul>
+                                <li>
+                                    <button>
+                                        <span>Suscribirme</span> 
+                                    </button>
+                                </li>
+                                <li>
+                                    <button>
+                                        <span>Navegar sin costo</span> 
+                                    </button>
+                                </li>
+                                <li>
+                                    <button>
+                                        <span>Mas información</span> 
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
                 </section>
             </>
         );
