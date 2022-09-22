a407b3b5275f548f2c035d37a8d1d72ebda3d2dc
iftikhar rasha <iftikhar.rasha@northsouth.edu>
2022-09-22 23:51:05 +0600

    podcast player page added

--- src/Pages/Radio/PodcastPlayer.js
+++ src/Pages/Radio/PodcastPlayer.js
@@ -1,101 +1,82 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { podcastEpisodes } from '../../Data/podcastEpisodes.js';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { radioData } from '../../Data/radioData';
-import play_button from '../../Image/play_button.svg';
-import arrow_left from '../../Image/arrow_left.svg';
 import play_fill from '../../Image/play_fill.svg';
-import share_icon from '../../Image/share_icon.svg';
-import plus_icon from '../../Image/plus_icon.svg';
-import love_icon from '../../Image/love_icon.svg';
-import radio from '../../Image/RadioMockImages/radioDetailsLogo.png';
-import radioThumb from '../../Image/RadioMockImages/radioThumb.png';
+import Sound from '../../Image/Soundbutton.svg';
+import Love from '../../Image/Lovebutton.svg';
+import Share from '../../Image/Sharebutton.svg';
+import pause from '../../Image/Pausebutton.svg';
+import podcastPlayerThumb from '../../Image/RadioMockImages/podcastPlayerThumb.png';
 import RadioSlider from '../../Components/Custom/Sliders/RadioSlider.js';
 import adbanner3 from '../../Image/adbanner3.png';
 import adbanner4 from '../../Image/adbanner4.png';
+import arrow from '../../Image/arrow-left-white.svg';
 
 const PodcastPlayer = () => {
     const navigate = useNavigate();
+    const {id} = useParams();
 
     return (
         <>
-            <section className="hero detailsHero">
-                <div className="heroBg heroFixed" style={{backgroundImage: `url(${radioThumb})`}}>
+            <section className="hero detailsHero playerHero">
+                <div className="heroBg">
+                    <div className="list">
+                        <Link to="/" className="title" onClick={() => navigate(-1)}><img src={arrow} alt={arrow} />Podcast</Link>
+                    </div>
                     <div className="detailsHero__wrapper">
-                        <Link to="/" onClick={() => navigate(-1)}><img src={arrow_left} alt="close" className="close"/></Link>
                         <div className="detailsHero__wrapper__contents">
                             <div className="detailsHero__wrapper__contents__left">
-                                <img src={radio} alt="radio"/>
-                            </div>
-                            <div className="detailsHero__wrapper__contents__right">
+                                <img src={podcastPlayerThumb} alt="radio"/>
                                 <h1>
                                     Leyendas Legendarias
                                 </h1>
-                                <h5>Hermanos Sánchez</h5>
-                                <h4>3 temporadas - 2019 - México</h4> 
-                                <h2><span>Categorias:</span> Cuentos |  Crímenes reales | Comedia</h2>
+                            </div>
+                            <div className="detailsHero__wrapper__contents__right">
+                                <h5>Acerca de Leyendas Legendarias</h5>
                                 <p>Un podcast de comedia donde cada semana exploraremos historias de asesinos en serie, fantasmas y eventos históricos peculiares, notorios o fantásticos.</p>
+                                <h2><span>Categorias:</span> Cuentos |  Crímenes reales | Comedia</h2>
+                                <Link to={`/podcast/details/`+id}>
+                                    <button>
+                                            <img src={play_fill} alt="play_fill" />
+                                            <span>Más episodios</span> 
+                                    </button>
+                                </Link>
+                                
+                                <ul className="episodes">
+                                    <li className="episodes__single">
+                                        <div className="episodes__single__left">
+                                            <div className="episodes__single__left__thumb">
+                                                <img src={podcastPlayerThumb} alt="intro" className="thumb"/>
+                                            </div>
+                                        </div>
+                                        <div className="episodes__single__right">
+                                            <div className="episodes__single__right__info">
+                                                <h4>Trailer</h4>
+                                                <p>2 min 20 s</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='detailsHero__wrapper__settings'>
+                            <div className="left">
+                                <img src={pause} alt="pause" />
+                                <p><span>Ahora suena:</span> E 153 - Bonnie y Clyde Pt 1 | Leyendas Legendarias</p>
+                            </div>
+                            <div className="right">
+                                <img src={Sound} alt="Sound" className='sound'/>
+                                <img src={Love} alt="Love" />
+                                <img src={Share} alt="Share" />
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-            
-            <section className="relativeTop detailsTop2">
-                {/* Only for parallex! */}
-            </section>
 
             <section className="allEpisodes podcastDetails playerDetails">
-                <div className="allEpisodes__buttons">
-                    <ul>
-                        <li>
-                            <button>
-                                <img src={play_fill} alt="play_fill" />
-                                <span>Reproducir</span> 
-                            </button>
-                        </li>
-                        <li><img src={love_icon} alt="love" className="love"/></li>
-                        <li><img src={share_icon} alt="play" className="play"/></li>
-                        <li><img src={plus_icon} alt="add" className="plus"/></li>
-                    </ul>
-                </div>
                 <div className="allEpisodes__body">
                     <div className="allEpisodes__body__contents">
-                        <div className="allEpisodes__body__contents__title">
-                            <h2>Episodios</h2>
-                            <div className="input-group">
-                                <select className="custom-select" id="selectEpisode" defaultValue={1}>
-                                    <option value="1">De más reciente a más antiguo</option>
-                                    <option value="2">De más reciente a más antiguo 2</option>
-                                    <option value="3">De más reciente a más antiguo 3</option>
-                                    <option value="3">De más reciente a más antiguo 4</option>
-                                </select>
-                            </div>
-                        </div>
-                        <ul className="allEpisodes__body__contents__episodes">
-                            {
-                                podcastEpisodes.map((item) => (
-                                <div className="body__contents__episodes__single" key={item.id}>
-                                    <div className="body__contents__episodes__single__left">
-                                        <div className="body__contents__episodes__single__left__thumb">
-                                            <img src={item.thumbnail} alt="intro" className="thumb"/>
-                                        </div>
-                                    </div>
-                                    <div className="body__contents__episodes__single__right">
-                                        <div className="body__contents__episodes__single__right__info">
-                                            <h4>{item.name}</h4>
-                                            <p>{item.description}</p>
-                                        </div>
-                                        <div className="body__contents__episodes__single__right__time">
-                                            <span>{item.time}</span>
-                                            <img src={play_button} alt="play_button" className="thumb"/>
-                                        </div>
-                                    </div>
-                                </div>
-                                ))
-                            }
-                        </ul>
-                        
                         <div className="playerDetails__cards">
                             <div className="playerDetails__cards__slider">
                                 <div className="allEpisodes__body__contents__similar">
